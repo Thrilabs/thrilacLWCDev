dad9673bedca33d635684a2de84977a3c69515b7
Thri3 <117852990+Thri3@users.noreply.github.com>
2022-11-22 09:23:54 +0530

    helloWorld test

---
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -0,0 +1,17 @@
+import { LightningElement } from 'lwc';
+
+export default class HelloWorld extends LightningElement {
+
+ccl
+
+
+
+
+
+
+}
+
+// Write a message to the console.
+// console.log('hello world!');
+
+
